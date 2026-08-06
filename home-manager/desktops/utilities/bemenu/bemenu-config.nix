e917e38d1e41e9c48012e57e2b9d02fb4e1dee04
railgun210 <aday56709@gmail.com>
2026-08-05 22:28:31 -0500

    Fixed a bug where bemenu wouldn't load with the HM

--- home-manager/desktops/utilities/bemenu/bemenu-config.nix
+++ home-manager/desktops/utilities/bemenu/bemenu-config.nix
@@ -7,23 +7,37 @@
 
 let
   c = config.lib.stylix.colors;
+  bemenuSettings = {
+    tb = "#${c.base00}";
+    tf = "#${c.base0D}";
+    fb = "#${c.base00}";
+    ff = "#${c.base05}";
+    nb = "#${c.base00}";
+    nf = "#${c.base05}";
+    hb = "#${c.base0D}";
+    hf = "#${c.base00}";
+    fn = config.utils.fonts.describeFont config.utils.fonts.status;
+  };
 in
 {
   programs.bemenu = {
     enable = true;
 
-    settings = {
-      tb = lib.mkForce "#${c.base00}";
-      tf = lib.mkForce "#${c.base0D}";
-      fb = lib.mkForce "#${c.base00}";
-      ff = lib.mkForce "#${c.base05}";
-      nb = lib.mkForce "#${c.base00}";
-      nf = lib.mkForce "#${c.base05}";
-      hb = lib.mkForce "#${c.base0D}";
-      hf = lib.mkForce "#${c.base00}";
-      fn = lib.mkForce (config.utils.fonts.describeFont config.utils.fonts.status);
-    };
+    # Integrated Home Manager does not expose its shell session variables to
+    # the graphical NixOS session, so bmenu needs this environment.d entry to
+    # receive the same colors and font when launched from Hyprland.
+    settings = lib.mapAttrs (_: lib.mkForce) bemenuSettings;
   };
+
+  xdg.configFile."environment.d/20-bemenu.conf".text =
+    let
+      bemenuOpts = lib.cli.toCommandLineShell (optionName: {
+        option = if builtins.stringLength optionName > 1 then "--${optionName}" else "-${optionName}";
+        sep = null;
+        explicitBool = false;
+      }) config.programs.bemenu.settings;
+    in
+    "BEMENU_OPTS=${bemenuOpts}\n";
 
   home.packages = with pkgs; [ bemoji ];
 }
